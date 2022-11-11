7b33e0a6f06d9ffa825601e57221ee6246d2fab3
mclovincomdelas <vicenzomf@gmail.com>
2022-11-11 09:56:30 -0300

    commt

--- public/js/main.js
+++ public/js/main.js
@@ -1,49 +1,4 @@
-////////////////////////////////////////////
-
-const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-  ];
-  
-  const data = {
-    labels: labels,
-    datasets: [{
-      label: 'My First dataset',
-      backgroundColor: [ 
-      'rgb(255, 99, 132)',
-      'rgb(54, 162, 235)',
-      'rgb(255, 205, 86)',
-      ],
-      data: [0, 10, 5, 2, 20, 30, 45],
-    }]
-  };
-  
-  const config = {
-    type: 'doughnut',
-    data: data,
-    options: {
-      plugins: {
-        legend: {
-          labels: {
-            font: {
-              size: 32
-            }
-          }
-        }
-      }
-    }
-  };
-  
-  
-  const myChart = new Chart(
-    document.getElementById('myChart'),
-    config
-  );
-  
+ 
   ///////////////////////////////////////////////////////
   
   
@@ -61,7 +16,7 @@ const labels = [
         const li = document.getElementById("li1");
         li.classList.add("active");
   
-      } else if(str == "teste.html") {
+      } else if(str == "cad") {
   
         const li = document.getElementById("li2");
         li.classList.add("active");
